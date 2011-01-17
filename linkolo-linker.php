931a9dbed2234e042da2264c803017f92ef06720
Linkolo <Linkolo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-17 08:50:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/linkolo-plugin/trunk@333449 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ linkolo-linker.php
@@ -0,0 +1,122 @@
+<?php
+
+/*
+Plugin Name: Linkolo Linker
+Description: Plugin pozwalający na instalację systemu Linkolo.pl na stronach wordpress
+Author: Artur Pleskot
+Version: 1.1
+Author URI: http://seopower.pl/
+*/
+
+$spwr_file_name_label = "spwr_file_name";
+add_filter("the_content", 'swpr_show_links', 9);
+add_action('admin_menu', 'linkolo_admin');
+
+function swpr_show_links() {
+
+	global $spwr_file_name_label, $post;
+
+	$spwr_file = get_option($spwr_file_name_label);
+
+	if (is_singular() && is_file($spwr_file)) {
+		
+		require $spwr_file;
+		return spwrPrintArticle($post->post_content);
+		
+	} else return $post->post_content;
+	
+}
+
+function linkolo_admin() {
+
+	add_options_page('Linkolo Linker Options', 'Linkolo Linker', 'manage_options', 'my-unique-identifier', 'linkolo_linker_options');
+
+}
+
+function linkolo_linker_options() {
+
+	global $spwr_file_name_label;
+
+	$path_array = explode("/", dirname(__FILE__));
+	$count = count($path_array);
+	for	($i=array_search('wp-content', $path_array); $i <= $count; $i++) unset($path_array[$i]);
+	
+	$dirname = implode("/", $path_array);
+		
+    if ($dh = opendir($dirname)) {
+        while (($file = readdir($dh)) !== false) {
+        	
+        	if (preg_match("/^cl_[0-9a-z]{16}\.php$/", $file)) $suggested_file = $file;
+        	if (preg_match("/^cl_[0-9a-z]{16}$/", $file)) $suggested_folder = $file;
+        	
+        }
+        closedir($dh);
+    }
+	
+	if (!current_user_can('manage_options'))  {
+		wp_die( __('Niestety. Nie masz odpowiednich uprawnień do edycji tej strony.') );
+	}
+	  
+	
+	
+	if (!empty($_POST[$spwr_file_name_label])) {
+	 	$spwr_file_name_val = $_POST[$spwr_file_name_label];
+		update_option($spwr_file_name_label, $spwr_file_name_val);
+?>
+<div class="updated"><p><strong><?php _e('Zmiany zostały zapisane', 'linkolo_linker_options' ); ?></strong></p></div>
+<?php
+
+ 	} else {
+ 		
+ 		$spwr_file_name_val = get_option($spwr_file_name_label);
+ 		
+ 	}
+
+  
+?>
+
+<div class="wrap">
+<h2><?php _e('Linkolo Linker Options', 'linkolo_linker_options') ?></h2>
+<?php if (!empty($suggested_file)) : ?>
+<p>
+	Plugin Linkolo linker działa poprawnie z Wordpress 2.9.x<br />
+	<hr />
+	W folderze<br /><br />
+	<strong><?php echo $dirname; ?></strong><br /><br />
+	Znaleźliśmy plik<br /><br />
+	<strong><?php echo $suggested_file; ?></strong>.<br /><br />
+	Prawdopodobnie jest to twój plik instalacyjny. <br /><br />
+	Pamiętaj też, że folder z plikami danych musi mieć ustawione odpowiednie uprawnienia do zapisu (np. chmod 777).
+</p>
+<?php endif; ?>
+<form name="linkolo-form" method="post" action="">
+<?php if (!empty($suggested_file)) : ?>
+<input type="hidden" name="suggested_file" value="<?php echo $dirname.'/'.$suggested_file; ?>">
+<?php endif; ?>
+<p><?php _e("Ścieżka dostępu do pliku instalacyjnego Linkolo :", 'linkolo_linker_options'); ?> 
+<input type="text" name="<?php echo $spwr_file_name_label; ?>" value="<?php echo $spwr_file_name_val; ?>" size="100">
+</p><hr />
+
+<p class="submit">
+<?php if (!empty($suggested_file)) : ?>
+<input type="button" onclick="insertfile(this.form)" class="button-primary" value="Użyj sugerowanego pliku" />
+<?php endif; ?>
+<input type="submit" name="Submit" class="button-primary" value="<?php esc_attr_e('Save Changes') ?>" />
+</p>
+
+</form>
+</div>
+<script type="text/javascript">
+	function insertfile(form) {
+		
+		form.<?php echo $spwr_file_name_label; ?>.value = form.suggested_file.value;
+		form.submit();
+		
+	}
+</script>
+	
+<?php 
+	
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,8 @@
+1. Wgrywamy plugin linkolo do folderu wp-content/plugins
+2. Wchodzimy do panelu administracyjnego bloga
+3. Klikamy menu "Wtyczki". 
+4. Szukamy wsród wy³¹czonych pluginów pluginu o nazwie Linkolo Linker i aktywujemy go klikaj¹c obok odnosnik "Aktywuj"
+5. W menu "Ustawienia" klikamy odnoœnik "Linkolo Linker".
+6. System powinien znaleŸæ automatycznie pliki instalacyjne o czym nas poinformuje. Jeœli system dobrze znalaz³ pliki to wystarczy klikn¹æ przycisk "U¿yj sugerowanego pliku". I to wszystko.
+7. Jeœli sugerowany plik nie zosta³ odnaleziony lub jest b³êdnie odnaleziony, nale¿y podaæ pe³n¹ œcie¿kê dostêpu do pliku instalacyjnego systemu.
+8. Po podaniu poprawnej œciezki dostêpu do pliku instalacyjnego klikamy przycisk "Zapisz zmiany". That's all.
